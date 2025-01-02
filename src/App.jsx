cb03682fb4e411289da959c79dba7021fd32ce7d
vijay <omvijay44@gmail.com>
2025-01-02 20:47:37 +0530

    tostar added...

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './app/router/index';
 import Navbar from './app/components/pages/Navbar';
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
+  const notify = () => toast("Wow so easy!");
   return (
     <BrowserRouter>
       <Navbar />
+      <div>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>
       <Router />
     </BrowserRouter>
   );
