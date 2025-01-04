8e0a273b10acab3d7214c622b344ca0d07893727
vijay <omvijay44@gmail.com>
2025-01-04 21:32:12 +0530

    added tostift for messages..

--- src/app/components/pages/ResetPasswordForm.jsx
+++ src/app/components/pages/ResetPasswordForm.jsx
@@ -1,29 +1,25 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ResetPasswordRequest = () => {
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setMessage('');
-    setError('');
 
     try {
       const response = await axios.get(
         `http://localhost:9091/api/v1/home/send-email-reset?email=${email}`
       );
-      setMessage(response.data.message); // Assuming response has a message field
+      toast.success(response.data.message);
       setLoading(false);
     } catch (err) {
-      setError(err.response ? err.response.data.errorMessage : 'Failed to send email');
+      toast.error(err.response ? err.response.data.errorMessage : 'Failed to send email');
       setLoading(false);
     }
   };
@@ -34,8 +30,6 @@ const ResetPasswordRequest = () => {
         <div className="col-md-6">
           <div className="card shadow p-4">
             <h2 className="card-title text-center mb-4">Forgot Password</h2>
-            {message && <p className="text-success">{message}</p>}
-            {error && <p className="text-danger">{error}</p>}
             <form onSubmit={handleSubmit}>
               <div className="form-group">
                 <label>Email:</label>
@@ -62,6 +56,7 @@ const ResetPasswordRequest = () => {
           </div>
         </div>
       </div>
+      <ToastContainer /> {/* Add ToastContainer to render the toasts */}
     </div>
   );
 };
